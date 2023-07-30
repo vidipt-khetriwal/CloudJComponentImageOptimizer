d7086a695c6eb6904bf81a5060b18f1fa52d0037
Vidipt <vidipt.khetriwal2020@vitstudent.ac.in>
2023-07-30 19:16:35 +0530

    redux set-up

---
+++ Documents/AirportProject/MIS/FrontendWebsite/MISWebApp/src/Reducers/rootReducer.jsx
@@ -0,0 +1,7 @@
+const initState = {
+  value: 0,
+};
+
+export const rootReducer = (state = initState, action) => {
+  return state;
+};
--- Documents/AirportProject/MIS/FrontendWebsite/MISWebApp/src/main.jsx
+++ Documents/AirportProject/MIS/FrontendWebsite/MISWebApp/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+//redux imports
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import RootReducer from "./Reducers/rootReducer.jsx";
+
+const store = createStore(RootReducer);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
